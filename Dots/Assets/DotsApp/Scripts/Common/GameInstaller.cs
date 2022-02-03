429bfd0d47765d953392a082cef09598bf19c441
Maxim Gulik <maxim.gulik@ilogos.biz>
2022-02-03 18:24:33 -0500

    Add game controller

--- Dots/Assets/DotsApp/Scripts/Common/GameInstaller.cs
+++ Dots/Assets/DotsApp/Scripts/Common/GameInstaller.cs
@@ -1,6 +1,3 @@
-using Dots.Infra.AC;
-using Dots.Infra.Assets;
-using Dots.Infra.Utils;
 using UnityEngine;
 using Zenject;
 
@@ -11,22 +8,7 @@ namespace DotsApp.Common
     {
         public override void InstallBindings()
         {
-            Container.Bind<IMessageBus>().To<MessageBus>().AsSingle();
-            Container.Bind<IDataBucket>().To<DataBucket>().AsSingle();
-
-            Container.Bind<ILoadSceneCommand>().To<LoadSceneCommand>().AsTransient();
-            Container.Bind<IUnloadSceneCommand>().To<UnloadSceneCommand>().AsTransient();
-
-            Container.Bind<IAssetLoader>().To<AddressablesAssetLoader>().AsTransient();
-
-            Container.Bind<ICreateControllerCommand>().To<CreateControllerCommand>().AsTransient();
-            Container.Bind<IInstantiateActorCommand>().To<InstantiateActorCommand>().AsTransient();
-            Container.Bind<IGetActorCommand>().To<GetActorCommand>().AsTransient();
-
-            Container.Bind(typeof(IApplicationSupport), typeof(IApplicationObserver), typeof(ICoroutineRunner))
-                .To<ApplicationSupport>()
-                .FromNewComponentOnNewGameObject()
-                .AsSingle();
+            Container.Bind<GameRoot.Controller>().AsTransient();
         }
     }
 }
